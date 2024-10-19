9ce8d240987c2fb3b2a863959dc20619443ec49d
Chia Chin Tat <119513588+Junda10@users.noreply.github.com>
2024-10-20 00:32:49 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,6 +5,7 @@
 # The train folder and test folder are separated. The former is used to fit the model, and the latter is used to predict. The stage=Test setting has been added in ImageDataset and DataModule.
 
 # %%
+pip install torch
 import os
 import random
 import pandas as pd
